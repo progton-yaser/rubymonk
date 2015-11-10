9d6c97ec20159b1b8165791aa8c7e6f7f304ece8
mohamedyaser <yaser@progton.com>
2015-11-10 23:07:55 +0530

    mansoor bhai done with basics stringsgit status

---
+++ strings_basic.rb
@@ -0,0 +1,19 @@
+puts 'Ruby'+'Monk'  # This string concatinates & the result is RubyMonk!!
+puts "November".length  # By using this command we can determine the length of the word in a string!!
+
+a = 5
+b = 6
+puts "The number #{a} is less than #{b}"  # This command helps to know the number is less than the given number. eg:) 5 is less than 6 !!
+
+puts "[Look:] I can’t believe it. [Don:] That is why you fail.".include? 'Don'  # include command helps to determine whether the given word/string is present in the sentence !!
+
+puts "Ruby is a beautiful language".start_with? "Ruby"  # start_with? command is used to check whether the given word starts with the same word not. if yes it prints true , if not false !!
+
+puts "Hey, how are you dude".end_with? "Ruby"   # end_with? command is used to check whether the given word ends with the same word not. if yes it prints true , if not false !!
+
+puts "I am the only person on earth".index 'p'  # index command is used to know the given letter is in which position/value  of the sentence. eg:) 'p' is in 14th position.
+
+puts "I AM HAPPIE FOR NOTHING".downcase  # downcase command is used to downcase the given sentence !!
+
+puts "HoW aRe mY fAmILy MeMbERS".swapcase  # swapping is a condition which changes the case of the words. eg:) a,b= b,a.
+ # Here swapcase is used to change the words that  are in lower(small) to higher(capital) priority. 
