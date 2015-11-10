10d20eb38b7867f3898a2a58eafc08ab0fa10b4e
mohamedyaser <yaser@progton.com>
2015-11-10 20:11:07 +0530

    mansoor bhai done with methods

--- objects.rb
+++ objects.rb
@@ -2,3 +2,4 @@ puts self  ## this will show case the main class which is ruby way of telling ev
 puts 1.odd? # check if number is odd ?
 puts 1.even? # check if number is even?
 puts "hello wporld"
+puts 1.next.next.next # method chaining example. if i do 1.next i get 2, if i do 1.next.next i get 3 & goes on.
---
+++ objects_meth.rb
@@ -0,0 +1,6 @@
+puts 1.methods # by using this command i can see diff. methods.
+puts 1.methods.sort #by adding including sort the methods are arranged in ascending order.
+puts ['rock','paper','scissors'].index('paper') # index method is used select particular item from the used list based on priority.
+puts ['rock','paper','scissors'].index('rock') # same comment as before.
+puts 2.between? 1,3 #between method is used to know whether the given number is present in between the numbers. 
+puts 4.between? 3,5 # same comment as below.
