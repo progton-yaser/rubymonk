3388041bf640aacbe6b3da379b62b8c2f562ca16
mohamedyaser <yaser@progton.com>
2015-11-10 18:55:58 +0530

    added hello world

--- objects.rb
+++ objects.rb
@@ -1,3 +1,4 @@
 puts self  ## this will show case the main class which is ruby way of telling everythings is an object
 puts 1.odd? # check if number is odd ?
 puts 1.even? # check if number is even?
+puts "hello wporld"
