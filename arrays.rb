ec815bed010cfc1cf54bc01fb1fa0c0004d560cb
mohamedyaser <yaser@progton.com>
2015-11-11 13:34:38 +0530

    done with arrays, can u once check for cond_loops_ruby.rb programs

---
+++ arrays.rb
@@ -0,0 +1,11 @@
+puts [1,2,3,4,5]  # By using this array coomand we can see numbers in vertical manner !!
+
+puts [1,'one',2, 3, 'four',4]  # By using this array command we can print numbers & strings in output !!
+
+puts [1, 2, 3, 4, 5][4]  # By using this array command we can locate the position of a number present in an array list !!!
+
+puts [1, 2, 3, 4, 5][-3]  # By using -ve number we can point the value of a number in an arrray. eg:) -3 points to '3' value!!
+
+puts [1,2,3,4] << "wow"  # By using '<<' command we can include string into an array. eg:) "wow" is added to array list !! 
+
+puts [1, 2, 3, 4, 5].push("progton")  # push is a command used to push a number into an array list!!
---
+++ cond_loops_ruby.rb
@@ -0,0 +1,23 @@
+#program 1:)-
+def ring(bell,n)
+  n.times do
+  puts bell.ring
+  end
+end
+
+
+
+
+
+
+#program 2:)-
+
+loop do
+  student.prepare
+  break if student.exams?
+end
+
+
+
+
+# I didnt get this how to get output, will u pls correct it send me back !!
