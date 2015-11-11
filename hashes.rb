0262df3f8bc9303ecce333fe09407f8b8fc9146c
mohamedyaser <yaser@progton.com>
2015-11-11 15:48:36 +0530

    done with hashes in ruby

---
+++ hashes.rb
@@ -0,0 +1,16 @@
+#program 1:)
+puts student_ages= { "Happy" =>3, "birth" =>4, "day" =>2 }
+# conclusion: By using hashes we can assign values to the keys. eg:) value '3' is assigned to 'happy' key !!
+
+#program 2:)
+restaurant_menu = {}
+puts restaurant_menu["cool drinks"]=4.5
+puts restaurant_menu["Tea"]=2
+# conclution: values are known for an item in the restaurant !!
+
+#program 3:)
+restaurant_menu = { "tea" => 3, "cool drinks" => 4, "Coffee" => 2 }
+restaurant_menu.each do | item, price |
+  puts "#{item}: $#{price}"
+end
+# conclution: assigning values/price for an particular item!!
---
+++ iteration.rb
@@ -0,0 +1,17 @@
+#program 1:)
+array = [1, 2, 3, 4, 5, 6]
+for i in array
+  puts i
+end
+
+# In this above program iteration method is used for repeatation of a process. eg:) for array=[1,2,3,4,5] the output is 1,2,3,4,5 !!  
+
+
+
+#program 2:)
+array = [1, 2, 3, 4, 5, 6]
+array.each do |i|
+  puts i
+end
+
+# array.each method accepts a block to which each element of the array is passed in turn !!
