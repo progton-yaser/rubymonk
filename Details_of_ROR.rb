c5adb3a3cf53060db5c94278917dd38fa08460fd
mohamedyaser <yaser@progton.com>
2015-11-27 18:23:53 +0530

    Details of SDLC, MVC, Rails Source Code

---
+++ Details_of_ROR.rb
@@ -0,0 +1,45 @@
+#=>
+#SDLC(Software development life cycle):-
+
+#Steps fo SDLC):-
+
+#Step 1) Requirement gathering!!
+#Step 2) Brainstorming!!
+#Step 3) Estimation!!
+#Step 4) Breakdown module!!
+#Step 5) Development. (Normal development & Test driven development)!!
+#Step 6) Quality analyst!!
+#Step 7) Deployment.(Staging & production)!!
+
+
+
+#Full stack developer involves two processes:)-
+# 1) Server side programmer (Programming language, Databases, Responses)!!
+# 2) Client side programmer (HTML, CSS, Jscript)!!
+
+
+
+# MVC(Model View Controller):-
+
+#Steps for MVC):-
+
+#Step 1) Client(Any Browser)!!
+#Step 2) Webserver!!
+#step 3) Dispatcher!!
+#Step 4) Controller to Model!!
+#Step 5) Model(Database)!!
+#Step 6) Model(Database) to Controller!!
+#Step 7) Controller to View!!
+#Step 8) View to Client!!
+
+
+#Rails Source Code):-
+
+# 1) Action Mailer -> Mailing libraries which handles all the email related stuff of our web application with little configuration!!
+# 2) Action Pack -> It performs two actions - Action Dispatch & Action Controller!!
+# 3) Action View -> View ERB (HTML code containing ruby action view - <%=  %>)!!
+# 4) Active Model -> It provides necessary validation method & some helper lib for model!!
+# 5) Active Job -> Library to handle backend jobs!! 
+# 6) Active Record -> Database ORM(Object Relational Mapping) & also conversion of ruby to SQL takes place. It is also an intermediate of Database & Rails!!
+# 7) Active Support -> Supporting libraries for rails & utilities!!
+# 8) Railties -> It is used to interlink between files & tables!!
